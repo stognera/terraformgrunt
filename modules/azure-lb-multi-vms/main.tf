2868948b5a04a2fb78e859283cb0abe13862ad7f
Adam Stogner <adam.stogner@homes.com>
2019-08-22 15:46:25 -0400

    test

--- modules/azure-lb-multi-vms/main.tf
+++ modules/azure-lb-multi-vms/main.tf
@@ -21,15 +21,16 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group
   location = var.location
 }
-/*
+
 resource "azurerm_storage_account" "stor" {
-  name                     = "${var.dns_name}stor"
-  location                 = "${var.location}"
-  resource_group_name      = "${azurerm_resource_group.rg.name}"
-  account_tier             = "${var.storage_account_tier}"
-  account_replication_type = "${var.storage_replication_type}"
+  name                     = var.dns_name
+  location                 = var.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_replication_type
 }
 
+/*
 resource "azurerm_availability_set" "avset" {
   name                         = "${var.dns_name}avset"
   location                     = "${var.location}"
