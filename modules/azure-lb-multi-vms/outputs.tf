0c1abc7960a5063258a04f941ece9bcd4676bbf4
Adam Stogner <adam.stogner@homes.com>
2019-08-26 15:29:27 -0400

    azure

--- modules/azure-lb-multi-vms/outputs.tf
+++ modules/azure-lb-multi-vms/outputs.tf
@@ -1,6 +1,14 @@
 #output "public_ip_address" {
 #  value = "${data.azurerm_public_ip.lbpip.ip_address}"
 #}
-output "public_ip_address" {
-  value = azurerm_lb.lb.frontend_ip_configuration
+#output "public_ip_address" {
+#  value = azurerm_lb.lb.frontend_ip_configuration
+#}
+
+#output "azurerm_public_ip_address" {
+#  description = "the ip address for the azurerm_lb_public_ip resource"
+#  value       = "${azurerm_public_ip.lb.*.ip_address}"
+#}
+output "vm_fqdn" {
+  value = "${azurerm_public_ip.lbpip.fqdn}"
 }
