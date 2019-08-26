6dcb8d02bd014065b12ce88c73074c7adef05a9c
Adam Stogner <adam.stogner@homes.com>
2019-08-26 13:29:26 -0400

    azure

--- modules/azure-lb-multi-vms/main.tf
+++ modules/azure-lb-multi-vms/main.tf
@@ -73,7 +73,6 @@ resource "azurerm_lb" "lb" {
   }
 }
 
-/*
 resource "azurerm_lb_backend_address_pool" "backend_pool" {
   resource_group_name = "${azurerm_resource_group.rg.name}"
   loadbalancer_id     = "${azurerm_lb.lb.id}"
@@ -91,6 +90,7 @@ resource "azurerm_lb_nat_rule" "tcp" {
   count                          = 2
 }
 
+/*
 resource "azurerm_lb_rule" "lb_rule" {
   resource_group_name            = "${azurerm_resource_group.rg.name}"
   loadbalancer_id                = "${azurerm_lb.lb.id}"
