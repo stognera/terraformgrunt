2081cd8dae93874b5775df121e643d449d1ad7a4
Adam Stogner <adam.stogner@homes.com>
2019-08-26 15:14:00 -0400

    azure

--- modules/azure-lb-multi-vms/outputs.tf
+++ modules/azure-lb-multi-vms/outputs.tf
@@ -2,5 +2,5 @@
 #  value = "${data.azurerm_public_ip.lbpip.ip_address}"
 #}
 output "public_ip_address" {
-  value = "${data.azurerm_lb.lb.id}"
+  value = "${data.azurerm_lb.LoadBalancerFrontEnd.public_ip_address_id}"
 }
